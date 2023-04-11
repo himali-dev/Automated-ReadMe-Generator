8f419c39d8a2aad3b2b804491dfb57f31f5f1d09
radhey-molsoft <himali.baps@gmail.com>
2023-04-11 13:46:24 -0400

    license added

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,52 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+function generateMarkdown(data) {
+
+  //returns a license badge based on which license is passed in
+
+
+  let licenseLink = "";
+  let licenseSelected = `${data.license}`;
+
+
+  if (licenseSelected === 'GNU AGPLv3') {
+    licenseSelected = 'GNUAGPLv3';
+    licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
+  };
+  if (licenseSelected === 'GNU GPLv3') {
+    licenseSelected = 'GNUGPLv3';
+    licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
+  };
+  if (licenseSelected === 'GNU LGPLv3') {
+    licenseSelected = 'GNULGPLv3';
+    licenseLink = 'https://choosealicense.com/licenses/lgpl-3.0/';
+  };
+  if (licenseSelected === 'Mozilla Public License 2.0') {
+    licenseSelected = 'MozillaPublicLicense2.0';
+    licenseLink = 'https://choosealicense.com/licenses/mpl-2.0/';
+  };
+  if (licenseSelected === 'Apache License 2.0') {
+    licenseSelected = 'ApacheLicense2.0';
+    licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
+  };
+  if (licenseSelected === 'MIT License') {
+    licenseSelected = 'MITLicense';
+    licenseLink = 'https://choosealicense.com/licenses/mit/';
+  };
+  if (licenseSelected === 'Boost Software License 1.0') {
+    licenseSelected = 'BoostSoftwareLicense1.0';
+    licenseLink = 'https://choosealicense.com/licenses/bsl-1.0/';
+  };
+  if (licenseSelected === 'The Unlicense') {
+    licenseSelected = 'TheUnlicense';
+    licenseLink = 'https://choosealicense.com/licenses/unlicense/';
+  };
+
+
+
+}
+
+
+
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
 
